cf9a2fe9a5deecd08890b622260d7af656507605
Bersabeh Asefa <bersabehasefa@interapts-macbook-pro-8.local>
2018-02-20 14:37:59 -0500

    Initial Commit

---
+++ Assessment 4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assessment 4
+//
+//  Created by Bersabeh Asefa on 2/20/18.
+//  Copyright © 2018 Bersabeh Asefa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
